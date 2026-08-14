392caa92f7b527b7a985147d1ba7f65877c3917a
biaconradoo <bialconrado@gmail.com>
2026-08-13 21:27:49 -0300

    Variáveis inteiras

---
+++ 01 - variáveis/var_desafio01.py
@@ -0,0 +1,6 @@
+# Desafio 1
+Largura = 85
+comprimento = 70
+Área = Largura * comprimento
+print (Área)
+print ('Área é:', Área)
---
+++ 01 - variáveis/var_int.py
@@ -0,0 +1,31 @@
+# Autor Beatriz Conrado
+# Projeto: Estudando variáveis inteiras
+# Nome de variável não pode ter caracteres especiais
+# (ç ã á à )
+# não pode ser uma palavra reservada
+# (print if else elif for)
+# 
+
+
+# nome e tipo
+valor1 = 12
+valor2 = 22
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2 
+divisao = valor1 / valor2 
+print(soma)
+print (subtracao)
+print (multiplicacao)
+print (divisao)
+
+# No python eu posso usar tanto aspas simples 
+# quanto aspas duplas
+print ('A soma é:', soma)
+print ("A subtracao é:", subtracao)
+print ('A multiplicacao é:', multiplicacao)
+print ("A divisao é:", divisao)
+
+# Imprimindo todos os resultados em uma única linha
+print ('A soma é: ', soma, 'e a subtracao é:', subtracao)
+
