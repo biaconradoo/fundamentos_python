7b8b07b9d12e955851510ffb5d66658335532226
biaconradoo <bialconrado@gmail.com>
2026-08-19 21:35:29 -0300

    Add files via upload

--- 01 - variáveis/var_desafio01.py
+++ 01 - variáveis/var_desafio01.py
@@ -3,10 +3,7 @@
 
 #Taxa de câmbio = 5.18
 
-Dolar = float(input('digite o valor de Dolar'))
-
-Resultado = Real 
-
-Real = 5.18*Dolar
-
-print(f'o valor da conversão de dolar para real Brasileiro é {Real}')
+Real = float(input('digite o valor de Real'))
+Dolar = Real/5.18
+resultado = Dolar
+print (f' o valor em reais, é {Dolar}')
