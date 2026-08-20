e700a2052cc824e92837fd567af6f0812c4664db
biaconradoo <bialconrado@gmail.com>
2026-08-19 21:45:26 -0300

    Add files via upload

---
+++ 01 - variáveis/var_float.py
@@ -0,0 +1,16 @@
+# Autor: Beatriz Conrado
+# Projeto: Variáveis Decimais
+
+#ex:
+idade = 15 # variável inteira - integer
+altura = 1.70 # variável decimal - float
+peso = 60.0 # variável decimal - float
+nome = 'Beatriz' #variável texto - string
+
+print(' Meu nome é ', nome, ' minha altura é ',
+ altura, ' meu peso é ', peso, ' e tenho ', idade, ' anos ')
+
+# f-string
+print(f'Meu nome é {nome} e minha altura é {altura}')
+
+
